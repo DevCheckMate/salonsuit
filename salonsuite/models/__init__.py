7d4b3990cc95eae3117ffc54bb0d915fcaf1e01a
fillipeberssot95@hotmail.com <fillipeberssot95@hotmail.com>
2024-12-17 23:04:43 -0300

    criado as tabelas de users, user_status e user_group no pacote models, ja inicializado pelo alembic

--- salonsuite/models/__init__.py
+++ salonsuite/models/__init__.py
@@ -1,5 +1,8 @@
-from .service_category import ServiceCategory
 from .service import Service
+from .service_category import ServiceCategory
 from .status import Status
+from .user_group import UserGroup
+from .user_status import UserStatus
+from .users import Users
 
-__all__ = ['ServiceCategory', 'Service', 'Status']
+__all__ = ['ServiceCategory', 'Service', 'Status', 'UserGroup', 'UserStatus', 'Users']
--- salonsuite/models/user_group.py
+++ salonsuite/models/user_group.py
@@ -1,26 +1,18 @@
 from datetime import datetime
 from typing import Optional
 
-from sqlalchemy import ForeignKey, String, func
+from sqlalchemy import String, func
 from sqlalchemy.orm import Mapped, mapped_column
 
 from salonsuite.database.orm_registry import table_mapper
-from salonsuite.utils.enum import Status
 
 
 @table_mapper.mapped_as_dataclass
-class ServiceCategory:
-    __tablename__ = 'service_category'
+class UserGroup:
+    __tablename__ = 'user_group'
 
-    service_category_id: Mapped[int] = mapped_column(
-        init=False, primary_key=True
-    )
+    user_group_id: Mapped[int] = mapped_column(init=False, primary_key=True)
     name: Mapped[str] = mapped_column(String(100), nullable=False)
-    status_id: Mapped[int] = mapped_column(
-        ForeignKey('status.status_id'),
-        nullable=False,
-        default=Status.ATIVO.value,
-    )
     created_at: Mapped[datetime] = mapped_column(
         init=False, server_default=func.now()
     )
@@ -28,5 +20,5 @@ class ServiceCategory:
         init=False, server_default=func.now(), onupdate=func.now()
     )
     deleted_at: Mapped[Optional[datetime]] = mapped_column(
-        nullable=True, default=None
+        nullable=False, default=None
     )
