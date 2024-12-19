725ef607c0ebcc6faf227a905bbc969a81b9f214
XxDanX <dan@DESKTOP-4LEQDJB>
2024-12-19 19:23:12 -0300

    sem status

--- salonsuite/seeds/enterprise_seed.py
+++ salonsuite/seeds/enterprise_seed.py
@@ -1,5 +1,4 @@
 from salonsuite.models.enterprise import EnterPrise
-from salonsuite.models.status import Status
 from sqlalchemy.orm import Session
 from salonsuite.database.db_connection import engine
 
@@ -7,15 +6,11 @@ def seed_enterprises():
     # Adicionar Dados da Tabela Status
     with Session(engine) as session:
 
-        status = [Status(name="ATIVO"), Status(name="INATIVO")]
-
         enterprise = [
             EnterPrise(name='SalonSuit', cnpj=65919825000150, cellphone=62991189413,
             email='salonsuit@gmail.com', state='Goiás', city='Goiânia', cep=74890020,
             )
         ]
-        session.add_all(status)
-        session.commit()
         session.add_all(enterprise)
         session.commit()
 
