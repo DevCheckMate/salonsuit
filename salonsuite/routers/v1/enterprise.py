32aa9943d99c57f2a91f1543c74f6d477c4bc515
XxDanX <dan@DESKTOP-4LEQDJB>
2024-12-27 15:01:14 -0300

    Criação do create_enterprise e EnterPriseCreatSchema

--- salonsuite/routers/v1/enterprise.py
+++ salonsuite/routers/v1/enterprise.py
@@ -3,13 +3,74 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 from salonsuite.models.status import Status
+from salonsuite.utils.enum import Status as EnumStatus
 
 from salonsuite.database.db_connection import get_session
 from salonsuite.models.enterprise import EnterPrise
-from salonsuite.schemas.enterprise import EnterPriseSchemaPublic
+from salonsuite.schemas.enterprise import EnterPriseSchemaPublic, EnterPriseCreatSchema
+
 
 router = APIRouter()
 
+@router.post('', response_model=EnterPriseSchemaPublic)
+def create_enterprise(
+    body : EnterPriseCreatSchema, session : Session = Depends(get_session)
+    ):
+    new_enterprise = EnterPrise(
+        name = body.name,
+        cnpj = body.cnpj,
+        cellphone = body.cellphone,
+        email = body.email,
+        state = body.state,
+        city = body.city,
+        cep = body.cep,
+        status_id = body.status_id
+    )
+
+    current_enterprise = session.query(EnterPrise).all()
+    for enterprise in current_enterprise:
+        if enterprise.cellphone == body.cellphone:
+            raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail='cellphone already exist'
+        )
+        if enterprise.cnpj == body.cnpj:
+            raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail='cnpj already exist'
+        )
+        if enterprise.email == body.email:
+            raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail='Email already exist'
+        )
+
+    session.add(new_enterprise)
+    session.commit()
+    session.refresh(new_enterprise)
+
+    stmt = (
+        select(
+            EnterPrise.name,
+            EnterPrise.cnpj,
+            EnterPrise.cellphone,
+            EnterPrise.email,
+            EnterPrise.state,
+            EnterPrise.city,
+            EnterPrise.cep,
+            EnterPrise.status_id,
+            Status.name.label("status_name")
+        ).join(
+            Status, EnterPrise.status_id == Status.status_id
+            ).where(
+            EnterPrise.enterprise_id == new_enterprise.enterprise_id
+        )
+    )
+    new_enterprise_db = session.execute(stmt).mappings().one_or_none()
+
+    new_enterprise_data = EnterPriseSchemaPublic(**dict(new_enterprise_db))
+
+    return new_enterprise_data
 
 @router.get('/{enterprise_id}', response_model=EnterPriseSchemaPublic)
 def get_enterprise_by_id(
@@ -26,12 +87,12 @@ def get_enterprise_by_id(
         EnterPrise.city,
         EnterPrise.cep,
         EnterPrise.status_id,
-        Status.name.label("status_name"),
-        EnterPrise.created_at,
+        Status.name.label("status_name")
     ).join(
         Status, EnterPrise.status_id == Status.status_id
         ).where(
-        EnterPrise.enterprise_id == enterprise_id
+        EnterPrise.enterprise_id == enterprise_id,
+        EnterPrise.status_id != EnumStatus.DELETADO.value
         )
     
     enterprise_db = session.execute(stmt).mappings().one_or_none()
@@ -44,4 +105,3 @@ def get_enterprise_by_id(
     data = EnterPriseSchemaPublic(**dict(enterprise_db))
 
     return data
-
--- salonsuite/schemas/enterprise.py
+++ salonsuite/schemas/enterprise.py
@@ -1,8 +1,6 @@
-from datetime import datetime
 from pydantic import BaseModel
 
 class EnterPriseSchemaPublic(BaseModel):
-    enterprise_id : int
     name : str
     cnpj : str
     cellphone : str
@@ -12,4 +10,13 @@ class EnterPriseSchemaPublic(BaseModel):
     cep : str
     status_id : int
     status_name : str
-    created_at : datetime
+
+class EnterPriseCreatSchema(BaseModel):
+    name : str
+    cnpj : str
+    cellphone : str
+    email : str
+    state : str
+    city : str
+    cep : str
+    status_id : int
