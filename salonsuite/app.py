1ef16b9db4a9a3c7a79388bd3b2a088967dd5823
fillipeberssot95@hotmail.com <fillipeberssot95@hotmail.com>
2025-01-07 16:23:28 -0300

    Rota get (list_users) feita

--- salonsuite/app.py
+++ salonsuite/app.py
@@ -6,7 +6,9 @@ from salonsuite.routers import v1_routers
 def create_app() -> FastAPI:
     ...
     app = FastAPI(
-        title='Salon Suite', version='0.1.0', description='Gestor de Salão de Beleza e Barbearia'
+        title='Salon Suite',
+        version='0.1.0',
+        description='Gestor de Salão de Beleza e Barbearia',
     )
     app.include_router(v1_routers)
 
--- salonsuite/routers/v1/users.py
+++ salonsuite/routers/v1/users.py
@@ -1,6 +1,8 @@
+from datetime import date
 from http import HTTPStatus
+from typing import List
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
@@ -11,6 +13,107 @@ from salonsuite.models.users import Users
 from salonsuite.schemas.users import UsersCreateSchema, UsersSchemaPublic
 
 router = APIRouter()
+
+
+@router.get('', response_model=List[UsersSchemaPublic])
+def list_users(
+    page: int = Query(
+        default=1, ge=1, description='Número da pagina (começando de 1)'
+    ),
+    image_url: str = Query(default=None, description='Imagem do Usuário'),
+    email: str = Query(default=None, description='Email do usuário'),
+    name: str = Query(default=None, description='Nome do Usuário'),
+    group_id: int = Query(default=None, description='Aceita 1, 2, 3, 4 e 5'),
+    group_name: str = Query(
+        default=None, description='Nome do Grupo do usuário'
+    ),
+    cellphone: str = Query(default=None, description='Telefone do Usuário'),
+    pin: str = Query(default=None, description='CPF do Usuário'),
+    password: str = Query(default=None, description='Senha do Usuário'),
+    birthdate: date = Query(
+        default=None, description='Data de Aniversario do Usuário'
+    ),
+    instagram: str = Query(default=None, description='Instagram do Usuário'),
+    gender: str = Query(default=None, description='Gênero do Usuário'),
+    description: str = Query(default=None, description='Descrição do Usuário'),
+    status_id: int = Query(default=None, description='Aceita 1, 2, 3 e 4'),
+    status_name: str = Query(
+        default=None, description='Nome do Status do Usuário'
+    ),
+    session: Session = Depends(get_session),
+):
+    if page <= 0:
+        page = 1
+
+    limit = 3
+
+    offset = (page - 1) * limit
+
+    stmt = (
+        select(
+            Users.image_url,
+            Users.email,
+            Users.name,
+            Users.group_id,
+            UserGroup.name.label('group_name'),
+            Users.cellphone,
+            Users.pin,
+            Users.password,
+            Users.birthdate,
+            Users.instagram,
+            Users.gender,
+            Users.description,
+            Users.status_id,
+            UserStatus.name.label('status_name'),
+        )
+        .join(UserGroup, Users.group_id == UserGroup.user_group_id)
+        .join(UserStatus, Users.status_id == UserStatus.user_status_id)
+    )
+
+    if image_url:
+        stmt = stmt.where(Users.image_url == image_url)
+    if email:
+        stmt = stmt.where(Users.email == email)
+    if name:
+        stmt = stmt.where(Users.name == name)
+    if group_id:
+        stmt = stmt.where(Users.group_id == group_id)
+    if group_name:
+        stmt = stmt.where(UserGroup.name == group_name)
+    if cellphone:
+        stmt = stmt.where(Users.cellphone == cellphone)
+    if pin:
+        stmt = stmt.where(Users.pin == pin)
+    if password:
+        stmt = stmt.where(Users.password == password)
+    if birthdate:
+        stmt = stmt.where(Users.birthdate == birthdate)
+    if instagram:
+        stmt = stmt.where(Users.instagram == instagram)
+    if gender:
+        stmt = stmt.where(Users.gender == gender)
+    if description:
+        stmt = stmt.where(Users.description == description)
+    if status_id:
+        stmt = stmt.where(Users.status_id == status_id)
+    if status_name:
+        stmt = stmt.where(UserStatus.name == status_name)
+
+    stmt = stmt.offset(offset).limit(limit).order_by(Users.users_id)
+
+    users_db = session.execute(stmt).mappings().all()
+
+    if not users_db:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User Not Found'
+        )
+
+    data = list()
+
+    for item in users_db:
+        data.append(UsersSchemaPublic(**dict(item)))
+
+    return data
 
 
 @router.get('/{user_id}', response_model=UsersSchemaPublic)
