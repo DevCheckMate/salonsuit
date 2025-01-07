400ed55a1f76f6fd06181ae6e0376922895d1928
fillipeberssot95@hotmail.com <fillipeberssot95@hotmail.com>
2025-01-07 16:54:52 -0300

    Rota put (put_user_by_id) feito

--- salonsuite/routers/v1/users.py
+++ salonsuite/routers/v1/users.py
@@ -15,6 +15,57 @@ from salonsuite.schemas.users import UsersCreateSchema, UsersSchemaPublic
 router = APIRouter()
 
 
+@router.post('', response_model=UsersSchemaPublic)
+def create_user(
+    body: UsersCreateSchema, session: Session = Depends(get_session)
+):
+    new_user = Users(
+        image_url=body.image_url,
+        email=body.email,
+        name=body.name,
+        group_id=body.group_id,
+        cellphone=body.cellphone,
+        pin=body.pin,
+        password=body.password,
+        birthdate=body.birthdate,
+        instagram=body.instagram,
+        gender=body.gender,
+        description=body.description,
+        status_id=body.status_id,
+    )
+
+    session.add(new_user)
+    session.commit()
+    session.refresh(new_user)
+
+    stmt = (
+        select(
+            Users.image_url,
+            Users.email,
+            Users.name,
+            Users.group_id,
+            Users.cellphone,
+            Users.pin,
+            Users.password,
+            Users.birthdate,
+            Users.instagram,
+            Users.gender,
+            Users.description,
+            Users.status_id,
+            UserGroup.name.label('group_name'),
+            UserStatus.name.label('status_name'),
+        )
+        .join(UserGroup, Users.group_id == UserGroup.user_group_id)
+        .join(UserStatus, Users.status_id == UserStatus.user_status_id)
+        .where(Users.users_id == new_user.users_id)
+    )
+
+    new_user_db = session.execute(stmt).mappings().one_or_none()
+    new_user_data = UsersSchemaPublic(**dict(new_user_db))
+
+    return new_user_data
+
+
 @router.get('', response_model=List[UsersSchemaPublic])
 def list_users(
     page: int = Query(
@@ -152,28 +203,35 @@ def get_user_by_id(user_id: int, session: Session = Depends(get_session)):
     return data
 
 
-@router.post('', response_model=UsersSchemaPublic)
-def create_user(
-    body: UsersCreateSchema, session: Session = Depends(get_session)
+@router.put('/{user_id}', response_model=UsersSchemaPublic)
+def put_user_by_id(
+    user_id: int,
+    body: UsersCreateSchema,
+    session: Session = Depends(get_session),
 ):
-    new_user = Users(
-        image_url=body.image_url,
-        email=body.email,
-        name=body.name,
-        group_id=body.group_id,
-        cellphone=body.cellphone,
-        pin=body.pin,
-        password=body.password,
-        birthdate=body.birthdate,
-        instagram=body.instagram,
-        gender=body.gender,
-        description=body.description,
-        status_id=body.status_id,
-    )
+    stmt = select(Users).where(Users.users_id == user_id)
 
-    session.add(new_user)
+    user_db = session.execute(stmt).scalar_one_or_none()
+
+    if not user_db:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User Not Found'
+        )
+
+    user_db.image_url = body.image_url
+    user_db.email = body.email
+    user_db.name = body.name
+    user_db.group_id = body.group_id
+    user_db.cellphone = body.cellphone
+    user_db.pin = body.pin
+    user_db.password = body.password
+    user_db.birthdate = body.birthdate
+    user_db.instagram = body.instagram
+    user_db.gender = body.gender
+    user_db.description = body.description
+    user_db.status_id = body.status_id
+
     session.commit()
-    session.refresh(new_user)
 
     stmt = (
         select(
@@ -181,6 +239,7 @@ def create_user(
             Users.email,
             Users.name,
             Users.group_id,
+            UserGroup.name.label('group_name'),
             Users.cellphone,
             Users.pin,
             Users.password,
@@ -189,15 +248,14 @@ def create_user(
             Users.gender,
             Users.description,
             Users.status_id,
-            UserGroup.name.label('group_name'),
             UserStatus.name.label('status_name'),
         )
-        .join(UserGroup, Users.group_id == UserGroup.user_group_id)
         .join(UserStatus, Users.status_id == UserStatus.user_status_id)
-        .where(Users.users_id == new_user.users_id)
+        .join(UserGroup, Users.group_id == UserGroup.user_group_id)
+        .where(Users.users_id == user_id)
     )
+    user_update_db = session.execute(stmt).mappings().one_or_none()
 
-    new_user_db = session.execute(stmt).mappings().one_or_none()
-    new_user_data = UsersSchemaPublic(**dict(new_user_db))
+    data = UsersSchemaPublic(**dict(user_update_db))
 
-    return new_user_data
+    return data
